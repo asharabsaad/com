b0b8bb4200ab1eab68ed31c4a8ddb37a3d1187c5
asharabsaad <104360135+asharabsaad@users.noreply.github.com>
2022-05-01 00:15:35 +0530

    Add files via upload

---
+++ Reversing.java
@@ -0,0 +1,20 @@
+package com.asharab;
+
+import java.util.Scanner;
+
+public class Reversing {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int ans = 0;
+        while(n>0){
+            int rem = n % 10;
+            n = n / 10;
+
+            ans = ans * 10 + rem ;
+
+        }
+        System.out.println(ans);
+
+    }
+}
